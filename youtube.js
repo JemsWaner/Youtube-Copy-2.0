ccd1a7927f423a341e23e38236eb9f467576190f
JemsWaner <jemswaner@gmail.com>
2023-01-04 23:27:03 -0400

    changes specially in the toggle shorts function

--- youtube.js
+++ youtube.js
@@ -5,3 +5,12 @@ const menus = document.getElementById("menu"),
 menus.addEventListener("click", () => {
   asides.classList.toggle("deploy");
 });
+
+const expand = document.getElementById("expand-more");
+const clicks = document.getElementsByClassName("hidden");
+
+expand.addEventListener("click", () => {
+  for (const touch of clicks) {
+    touch.style.display = "block";
+  }
+});
